6287fce9c76d0ace1745a58d921c910a410427f1
Chris Yu <chris.yi@iag.com.au>
2023-11-26 11:52:01 +1100

    buttons switch

--- src/ControlPanel.jsx
+++ src/ControlPanel.jsx
@@ -1,11 +1,39 @@
 import { useEffect, useState } from 'react'
 
+const WINDOW_PERMISSION = {
+  PENDING: 'prompt',
+  DENIED: 'denied',
+  GRANTED: 'granted',
+}
+
 export const ControlPanel = () => {
   const [windowPosition, setWindowPosition] = useState({
     x: 0,
     y: 0,
   })
+  const [hasWindowPermission, setWindowPermission] = useState(false)
 
+  const getPermission = async () => {
+    await window.getScreenDetails()
+  }
+  const openNewWindow = async () => {
+    alert('new window')
+  }
+
+  // get permission status
+  useEffect(() => {
+    const getWindowPermissionStatus = async () => {
+      const { state } = await navigator.permissions.query({
+        name: 'window-management',
+      })
+
+      setWindowPermission(state === WINDOW_PERMISSION.GRANTED)
+    }
+
+    getWindowPermissionStatus()
+  }, [])
+
+  // repeatly get window position in the screen
   useEffect(() => {
     const getCirclePosition = () => {
       const x = window.screenX
@@ -22,10 +50,11 @@ export const ControlPanel = () => {
     <div className="control-panel">
       <div>x: {windowPosition.x}</div>
       <div>y: {windowPosition.y}</div>
-      <div className="buttons-container">
-        {/* <button onClick={getPermission}>Get Permission</button>
-        <button onClick={getScreenXandY}>Get screenX and screenY</button> */}
-      </div>
+      {!hasWindowPermission ? (
+        <button onClick={getPermission}>Get Permission</button>
+      ) : (
+        <button onClick={openNewWindow}>Open New Window</button>
+      )}
     </div>
   )
 }
